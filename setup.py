8cf36326ae0f145763dbaf0606fdcff68aa10cdf
mia.metzler <mia.metzler@bpm.ch>
2025-12-16 16:00:04 +0100

    Release 0.1.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="harvesterpy",
-    version="0.1.4",
+    version="0.1.5",
     author="bpmconsultag",
     description="A Python library to interface with SUSE Harvester HCI",
     long_description=long_description,
--- upload_pypi.sh
+++ upload_pypi.sh
@@ -7,8 +7,8 @@ python -m twine upload dist/*
 # For TestPyPI use the following commands:
 twine upload --repository testpypi dist/*
 # Install from test PyPI but prefer requirements from the official PyPI
-# pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple/ harvesterpy==0.1.4
+# pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple/ harvesterpy==0.1.5
 
 # For PyPI use the following commands:
 # twine upload dist/*
-# pip install harvesterpy==0.1.4
+# pip install harvesterpy==0.1.5
